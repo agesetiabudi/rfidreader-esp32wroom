4c278ecbb269dfbb64a28159ca523dff5411a2a1
Agesetiabudi <agesetiabudi16@gmail.com>
2024-11-10 12:35:21 +0700

    FEAT: add module DS3231

--- rfidwrrom/rfidwrrom.ino
+++ rfidwrrom/rfidwrrom.ino
@@ -2,6 +2,7 @@
 #include <MFRC522.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <RTClib.h>
 
 #define SS_PIN 10   
 #define RST_PIN 9   
@@ -11,6 +12,10 @@
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+RTC_DS3231 rtc;
+
+// Variabel global untuk menyimpan waktu
+DateTime sekarang;
 
 void setup() {
   Serial.begin(115200);      
@@ -21,12 +26,27 @@ void setup() {
   Wire.begin(SDA_PIN, SCL_PIN);
   lcd.init();
   lcd.backlight();
-  
+
+  if (!rtc.begin()) {
+    Serial.println("RTC tidak ditemukan!");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC kehilangan daya, set ulang waktu...");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
   tampilkanPesanAwal();
   Serial.println("Tempatkan kartu RFID dekat dengan modul...");
 }
 
 void loop() {
+  sekarang = rtc.now();  // Ambil waktu sekarang setiap loop
+  Serial.print("Waktu sekarang: ");
+  Serial.print(sekarang.timestamp(DateTime::TIMESTAMP_TIME));
+  // Serial.println(" (Asia/Jakarta)");
+
   if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     delay(50);
     return;
@@ -41,15 +61,14 @@ void loop() {
   lcd.setCursor(3, 4);
 
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-      lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
-      lcd.print(mfrc522.uid.uidByte[i], HEX);
+    lcd.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+    lcd.print(mfrc522.uid.uidByte[i], HEX);
   }
 
-  // Untuk Serial Monitor juga, tanpa spasi antar byte
   Serial.print("UID Kartu: ");
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
-      Serial.print(mfrc522.uid.uidByte[i], HEX);
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? "0" : "");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
   }
   Serial.println();
 
